d07480d6545283226212e8e09d4e1c080ac1f055
Orlando Joel Yagual Endara <DANIELCASTELAO\oyagualendara@nautilus31.ad.danielcastelao.org>
2019-10-30 11:52:05 +0100

    first commit

---
+++ boletin4_3/Boletin4_3.java
@@ -0,0 +1,16 @@
+
+package boletin4_3;
+
+
+public class Boletin4_3 {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Circulo circulo=new Circulo();
+        circulo.setRadio(40);
+System.out.println("El area es igual a "+circulo.calcularArea()+" y la longitud a "+circulo.calcularLonxitude());
+        
+    }
+    
+}
